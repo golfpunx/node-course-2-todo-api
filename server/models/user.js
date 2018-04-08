7ef74286f4a4b20e1a89a26ed9f352e234f8fa5e
Prakhan Jansawang <gooit12@gmail.com>
2018-04-08 16:51:46 +0700

    test commit from atom again

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,21 @@
 var mongoose = require('mongoose');
 
+// {
+//   email: 'prakhan@example.com',
+//   password: 'password',
+//   tokens: [{
+//     access: 'auth',
+//     token: 'palksjdlkfja;sdf;kj'
+//   }]
+// }
+
 var User = mongoose.model('Users', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
+    minlength: 1,
+    unique: true
   }
 });
 
